eecaf94dfce32824476ef8b2eb3a7ca1c5b3a024
NicoNico222 <wlsdnbilly@naver.com>
2026-07-16 13:57:51 +0900

    백엔드 주소 수정

--- src/api/http.js
+++ src/api/http.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const baseURL =
   import.meta.env.VITE_API_BASE_URL ||
-  'https://port-0-localhub-backend-mpdltcf63345e3f5.sel3.cloudtype.app'
+  'https://localhub-backend-rgl5.onrender.com'
 
 const http = axios.create({
   baseURL,
